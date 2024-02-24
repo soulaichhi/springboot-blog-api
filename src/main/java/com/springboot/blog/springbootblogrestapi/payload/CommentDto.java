de67632256c210497898934ecbe22f9ae1aa662d
soulaichhi <chhibou.soulaimane@gmail.com>
2024-02-24 13:08:43 +0100

    Validate Create Comment and Update Comment REST API Request

--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
@@ -1,11 +1,19 @@
 package com.springboot.blog.springbootblogrestapi.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "name should not be empty")
     private String name;
+    @NotEmpty(message = "email should not be empty")
+    @Email
     private String email;
+    @NotEmpty(message = "body should not be empty")
+    @Size(min = 10, message = "body should be at least 10 characters")
     private String body;
 }
